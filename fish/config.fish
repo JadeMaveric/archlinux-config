6e6e801b8b2b7c5ac871078168f806af50c09bc3
julius <julius@127>
2020-01-01 15:35:28 +0530

    Happy New Year

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,6 @@
 kitty + complete setup fish | source
+# set fish_function_path $fish_function_path "/usr/lib/python3.7/site-packages/powerline/bindings/fish"
+# powerline-setup
+
+#PATHS
+set PATH ~/.npm-global/bin $PATH
